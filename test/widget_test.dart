bd943ba2626b07e587cd093ce5acdbea964984f8
Fauzi <fauzibinfaisal@gmail.com>
2026-05-13 09:13:57 +0700

    setup auto deploy web & fix testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,11 +2,20 @@
 library;
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logic_lab/core/di/injection.dart';
 import 'package:logic_lab/main.dart';
 
+// test/widget_test.dart
+
 void main() {
+  setUp(() {
+    sl.reset();
+    configureDependencies();
+  });
+
   testWidgets('App smoke test', (tester) async {
     await tester.pumpWidget(const LogicLabApp());
     expect(find.text('LogicLab'), findsOneWidget);
   });
 }
+
